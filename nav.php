6ea7cc180afa25a7089d4eaed0a95a831cfc9abe
awaw94 <40030546+awaw94@users.noreply.github.com>
2018-08-14 20:59:15 +0700

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,8 @@
+<!-- membuat isi navigasi -->
+<nav>
+  <ul>
+  <li><a href="login.php" title="Home">Logout</a></li>
+    <li><a href="index.php" title="Admin">Home</a></li>
+      
+  </ul>
+</nav>
